6e3a690eaf039f042e199c9493264c10f6c0d520
Bryan Garcia <ing.bryangaco@gmail.com>
2024-02-10 15:41:03 -0500

    Creamos tareas por defecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -22,3 +22,4 @@ function dev() {
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series( css, dev);
