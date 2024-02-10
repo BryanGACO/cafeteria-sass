b48c6c59dd6effa6d18b7e8500ffbc7c571cc6ca
Bryan Garcia <ing.bryangaco@gmail.com>
2024-02-10 15:09:31 -0500

    Agreamos gulpfile y app.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
-function tarea(done){
-    console.log('Desde mi primer tarea...');
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+    // Compilar sass
+    // 1. Identificar archivo, 2 Compilarlo, 3 guardar el .css
+
+    // Funcion de gulp que permite identificar un archivo
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'))
 
     done();
 }
 
-exports.tarea = tarea;
+exports.css = css;
