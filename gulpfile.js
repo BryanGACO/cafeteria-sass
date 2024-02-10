c0eae0a7155499dc51c4c329304a50c75724d33b
Bryan Garcia <ing.bryangaco@gmail.com>
2024-02-10 15:35:29 -0500

    Agregamos watch al archivo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done) {
     // Compilar sass
@@ -7,10 +9,16 @@ function css(done) {
 
     // Funcion de gulp que permite identificar un archivo
     src('src/scss/app.scss')
-        .pipe(sass())
+        .pipe(sass()) //.pipe(sass({outputStyle: 'compressed'}}))
+        .pipe(postcss([ autoprefixer() ]))
         .pipe(dest('build/css'))
 
     done();
 }
 
+function dev() {
+    watch('src/scss/app.scss', css);
+}
+
 exports.css = css;
+exports.dev = dev;
