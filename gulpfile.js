a8c06122024b9e4d909e6be1111ace0dac0597a8
Bryan Garcia <ing.bryangaco@gmail.com>
2024-02-18 18:47:58 -0500

    Agregamos sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const imagemin = require('gulp-imagemin');
@@ -16,8 +17,10 @@ function css(done) {
 
     // Funcion de gulp que permite identificar un archivo
     src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass()) //.pipe(sass({outputStyle: 'compressed'}}))
         .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'))
 
     done();
